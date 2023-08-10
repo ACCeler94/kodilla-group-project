961148a5649afa435316750b258df85dc6cfbfa6
Artur C <araghast94@icloud.com>
2023-08-10 19:37:39 +0200

    Add different colored stars on user rated products and when the user hovers on stars

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -8,7 +8,7 @@ import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
 import StarRating from '../StarRating/StarRating';
 
-const ProductBox = ({ name, price, promo, stars, imgSrc, oldPrice }) => (
+const ProductBox = ({ name, price, promo, stars, userRating, imgSrc, oldPrice }) => (
   <div className={styles.root}>
     <div className={styles.photo}>
       {promo && <div className={styles.sale}>{promo}</div>}
@@ -22,7 +22,9 @@ const ProductBox = ({ name, price, promo, stars, imgSrc, oldPrice }) => (
     </div>
     <div className={styles.content}>
       <h5>{name}</h5>
-      <StarRating stars={stars} />
+      <div className={styles.starsWrapper}>
+        <StarRating stars={stars} userRating={userRating} />
+      </div>
     </div>
     <div className={styles.line}></div>
     <div className={styles.actions}>
@@ -51,6 +53,7 @@ ProductBox.propTypes = {
   price: PropTypes.number,
   promo: PropTypes.string,
   stars: PropTypes.number,
+  userRating: PropTypes.number,
   imgSrc: PropTypes.string,
   oldPrice: PropTypes.number,
 };
--- src/components/common/StarRating/StarRating.js
+++ src/components/common/StarRating/StarRating.js
@@ -7,15 +7,27 @@ import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 
 const StarRating = ({ stars, userRating }) => {
+  const checkUserRating = i => {
+    if (userRating) {
+      if (i <= userRating) {
+        return <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>;
+      } else {
+        return <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>;
+      }
+    } else {
+      if (i <= stars) {
+        return <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>;
+      } else {
+        return <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>;
+      }
+    }
+  };
+
   return (
     <div className={styles.root}>
       {[1, 2, 3, 4, 5].map(i => (
-        <a key={i} href='#'>
-          {i <= stars ? (
-            <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-          ) : (
-            <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-          )}
+        <a key={i} href='#' className={userRating ? styles.userRated : ''}>
+          {checkUserRating(i)}
         </a>
       ))}
     </div>
