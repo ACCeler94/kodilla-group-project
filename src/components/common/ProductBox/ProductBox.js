3d1116f896e2b97a5d92d7750904fb9b686a710e
Artur C <araghast94@icloud.com>
2023-08-10 16:26:40 +0200

    Add rating stars as separate component

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -3,13 +3,10 @@ import PropTypes from 'prop-types';
 
 import styles from './ProductBox.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faStar,
-  faExchangeAlt,
-  faShoppingBasket,
-} from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faExchangeAlt, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
+import StarRating from '../StarRating/StarRating';
 
 const ProductBox = ({ name, price, promo, stars, imgSrc, oldPrice }) => (
   <div className={styles.root}>
@@ -25,17 +22,7 @@ const ProductBox = ({ name, price, promo, stars, imgSrc, oldPrice }) => (
     </div>
     <div className={styles.content}>
       <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
+      <StarRating stars={stars} />
     </div>
     <div className={styles.line}></div>
     <div className={styles.actions}>
---
+++ src/components/common/StarRating/StarRating.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './StarRating.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+
+const StarRating = ({ stars, userRating }) => {
+  return (
+    <div className={styles.root}>
+      {[1, 2, 3, 4, 5].map(i => (
+        <a key={i} href='#'>
+          {i <= stars ? (
+            <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+          ) : (
+            <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+          )}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+StarRating.propTypes = {
+  stars: PropTypes.number,
+  userRating: PropTypes.number,
+};
+
+export default StarRating;
