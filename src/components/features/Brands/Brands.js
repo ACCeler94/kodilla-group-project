d66adde8cdc56e84dfe00b29fea878adb74da36f
Adrianna Nowak <adriannanowakk@gmail.com>
2023-08-16 21:23:35 +0200

    add images from initialState to the Brand component and style them

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -1,27 +1,41 @@
 import styles from './Brands.module.scss';
 import React from 'react';
 import Button from '../../common/Button/Button';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faLessThan, faGreaterThan } from '@fortawesome/free-solid-svg-icons';
+import { useSelector } from 'react-redux';
 
-const Brands = props => (
-  <div className={styles.root}>
-    <div className='container'>
-      <div className={styles.brandsContainer}>
-        <div className={styles.brandsLine}></div>
-        <div className={styles.buttons}>
-          <Button variant='small' className={styles.leftButton}>
-            <FontAwesomeIcon icon={faLessThan} />
-          </Button>
-          <Button variant='small' className={styles.rightButton}>
-            <FontAwesomeIcon icon={faGreaterThan} />
-          </Button>
+const Brands = () => {
+  const products = useSelector(state => state.products);
+
+  const brandImages = products.map(product =>
+    product.brand ? product.brand.img : null
+  ); // Sprawdzamy, czy brand istnieje, zanim uzyskamy dostęp do img
+
+  const filteredBrandImages = brandImages.filter(imgSrc => imgSrc !== null);
+
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className={styles.brandsContainer}>
+          <div className={styles.brandsLine}></div>
+          <div className={styles.buttons}>
+            <Button variant='small' className={styles.leftButton}>
+              <FontAwesomeIcon icon={faLessThan} className={styles.brandIcon} />
+            </Button>
+            <Button variant='small' className={styles.rightButton}>
+              <FontAwesomeIcon icon={faGreaterThan} className={styles.brandIcon} />
+            </Button>
+          </div>
+          <div className={styles.brandImages}>
+            {' '}
+            {filteredBrandImages.map((img, index) => (
+              <img key={index} src={img} alt={`Brand ${index}`} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Brands;
