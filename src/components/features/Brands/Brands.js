18dd4bc88cca2182af1e8aa9f909e9e61a4be23c
Adrianna Nowak <adriannanowakk@gmail.com>
2023-08-16 16:16:13 +0200

    create Brands component

---
+++ src/components/features/Brands/Brands.js
@@ -0,0 +1,10 @@
+import styles from './Brands.module.scss';
+import React from 'react';
+
+const Brands = props => (
+  <div className={styles.root}>
+    <div className='container'>lolo</div>
+  </div>
+);
+
+export default Brands;
