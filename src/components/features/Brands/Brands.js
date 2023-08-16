5a416e588fc5acbe7595a05d20c57c323f4c46ec
Adrianna Nowak <adriannanowakk@gmail.com>
2023-08-16 19:01:35 +0200

    add styles, button and lines to the Brands component

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -1,9 +1,26 @@
 import styles from './Brands.module.scss';
 import React from 'react';
+import Button from '../../common/Button/Button';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faLessThan, faGreaterThan } from '@fortawesome/free-solid-svg-icons';
 
 const Brands = props => (
   <div className={styles.root}>
-    <div className='container'>lolo</div>
+    <div className='container'>
+      <div className={styles.brandsContainer}>
+        <div className={styles.brandsLine}></div>
+        <div className={styles.buttons}>
+          <Button variant='small' className={styles.leftButton}>
+            <FontAwesomeIcon icon={faLessThan} />
+          </Button>
+          <Button variant='small' className={styles.rightButton}>
+            <FontAwesomeIcon icon={faGreaterThan} />
+          </Button>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
