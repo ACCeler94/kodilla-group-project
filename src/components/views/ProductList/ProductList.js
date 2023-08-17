9508f69a6694867ae13fe74eb440a95f2167fbfa
Artur C <araghast94@icloud.com>
2023-08-17 22:12:31 +0200

    Add basic template for ProductList

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -2,7 +2,30 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+const ProductList = () => {
+  return (
+    <div className={styles.root}>
+      <div className='row'>
+        <div className={`col-12 ${styles.bannerContainer}`}>
+          <h2>Banner</h2>
+        </div>
+      </div>
+      <div className='row'>
+        <div className={`col-9 ${styles.productListWrapper}`}>
+          <h3>Product List</h3>
+        </div>
+        <div className={`col-3 ${styles.filtersWrapper}`}>
+          <h3>Filters</h3>
+        </div>
+      </div>
+      <div className='row'>
+        <div className={`col-12 ${styles.brandsContainer}`}>
+          <h3>Brands</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 // ProductList.propTypes = {};
 
