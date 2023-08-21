4b98321a6f0c514ee8e50914307bef0137b18489
Darek Biernat <kaiju83@outlook.com>
2023-08-21 21:10:37 +0200

    Fix RWD styles and add Swipeable for right column

--- src/components/features/Promoted/Promoted.js
+++ src/components/features/Promoted/Promoted.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Promoted.module.scss';
 import ProductBox from '../../common/ProductBox/ProductBox';
 import { useSelector } from 'react-redux';
@@ -11,6 +11,7 @@ import {
   faArrowRight,
   faShoppingBasket,
 } from '@fortawesome/free-solid-svg-icons';
+import Swipeable from '../../views/Swipeable/Swipeable';
 
 const Promoted = () => {
   /* This selector is only to display picture for presentation into right column */
@@ -18,11 +19,25 @@ const Promoted = () => {
 
   const [promotedProd] = useSelector(getPromoted);
 
+  const [newPic, setNewPic] = useState(0);
+
+  const rightAction = () => {
+    if (newPic !== 0) {
+      setNewPic(newPic - 1);
+    }
+  };
+
+  const leftAction = () => {
+    if (newPic !== 7) {
+      setNewPic(newPic + 1);
+    }
+  };
+
   return (
     <div>
       <div className='container mt-4'>
         <div className='row text-center '>
-          <div className='col-12 col-md-4 position-relative '>
+          <div className='d-md-none d-lg-block col-md-4 position-relative '>
             <div className={'row ' + styles.hotDiv}>
               <div className={'col ' + styles.dealsDiv}>
                 <p>HOT DEALS</p>
@@ -65,39 +80,51 @@ const Promoted = () => {
 
             <ProductBox {...promotedProd} />
           </div>
-          <div className='col-12 col-md-8  position-relative p-0'>
-            <img
-              className={styles.promPic}
-              src={picForPresentation[7].imgSrc}
-              alt='armchair'
-            />
-            <div className={styles.innerBelt}>
-              <h1 className={styles.beltTitl}>
-                INDOOR <strong>FURNITURE</strong>
-              </h1>
-              <h5 className={styles.beltTitl}>SAVE UP TO 50% OF ALL FURNITURE</h5>
-            </div>
-            <Button variant='outline' className={styles.promButt}>
-              SHOP NOW
-            </Button>
-            <div className='row'>
-              <div className='col pr-0 '>
-                <Button noHover variant='outline' className={styles.arrLe}>
-                  <FontAwesomeIcon
-                    icon={faArrowLeft}
-                    className={styles.butIcon}
-                  ></FontAwesomeIcon>
-                </Button>
+          <div className='col-12 col-lg-8  position-relative p-0'>
+            <Swipeable leftAction={leftAction} rightAction={rightAction}>
+              <img
+                className={styles.promPic}
+                src={picForPresentation[newPic].imgSrc}
+                alt='armchair'
+              />
+              <div className={styles.innerBelt}>
+                <h1 className={styles.beltTitl}>
+                  INDOOR <strong>FURNITURE</strong>
+                </h1>
+                <h5 className={styles.beltTitl}>SAVE UP TO 50% OF ALL FURNITURE</h5>
               </div>
-              <div className='col pl-0'>
-                <Button noHover variant='outline' className={styles.arrLe}>
-                  <FontAwesomeIcon
-                    icon={faArrowRight}
-                    className={styles.butIcon}
-                  ></FontAwesomeIcon>
-                </Button>
+              <Button variant='outline' className={styles.promButt}>
+                SHOP NOW
+              </Button>
+              <div className='row'>
+                <div className='col pr-0 '>
+                  <Button
+                    noHover
+                    variant='outline'
+                    className={styles.arrLe}
+                    onClick={() => rightAction()}
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowLeft}
+                      className={styles.butIcon}
+                    ></FontAwesomeIcon>
+                  </Button>
+                </div>
+                <div className='col pl-0'>
+                  <Button
+                    noHover
+                    variant='outline'
+                    className={styles.arrLe}
+                    onClick={() => leftAction()}
+                  >
+                    <FontAwesomeIcon
+                      icon={faArrowRight}
+                      className={styles.butIcon}
+                    ></FontAwesomeIcon>
+                  </Button>
+                </div>
               </div>
-            </div>
+            </Swipeable>
           </div>
         </div>
       </div>
